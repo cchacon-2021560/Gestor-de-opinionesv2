d2bb76abd995adf78d2348852f239ad006cd1f3a
cchacon-2021560 <cchacon-2021560@kinal.edu.gt>
2026-02-23 17:23:45 -0600

    Fix: Correción de lógica de registro para poder reactivar un cuenta 'eliminada'

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,13 +1,41 @@
 import { registerHelper, verifyAccountHelper, loginHelper } from '../../helpers/auth.helper.js';
+import User from '../users/user.model.js';
+import { hash } from 'argon2';
 
 export const register = async (req, res) => {
     try {
-        const userData = req.body;
-        
-        
+        const { email, username, password, ...rest } = req.body;
         const profilePictureUrl = req.file ? req.file.path : null;
 
-        const result = await registerHelper(userData, profilePictureUrl);
+        const existingUser = await User.findOne({ 
+            $or: [{ email }, { username }] 
+        });
+
+        if (existingUser) {
+            if (existingUser.status === false) {
+                existingUser.status = true;
+                existingUser.password = await hash(password); 
+                existingUser.username = username;
+                existingUser.email = email;
+                existingUser.profilePicture = profilePictureUrl;
+                Object.assign(existingUser, rest);
+
+                await existingUser.save();
+
+                return res.status(200).json({
+                    success: true,
+                    message: "Cuenta existente reactivada con éxito. Por favor, verifica tu correo.",
+                    user: existingUser
+                });
+            } else {
+                return res.status(400).json({
+                    success: false,
+                    message: "El correo o username ya está en uso por una cuenta activa."
+                });
+            }
+        }
+
+        const result = await registerHelper(req.body, profilePictureUrl);
 
         return res.status(201).json({
             success: true,
@@ -21,7 +49,7 @@ export const register = async (req, res) => {
             message: error.message || 'Error al registrar el usuario'
         });
     }
-};
+}
 
 export const verifyAccount = async (req, res) => {
     try {
