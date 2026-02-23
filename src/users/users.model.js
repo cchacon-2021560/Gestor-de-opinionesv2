545344346a90fd854c98f2b9e4eca4839494b495
cchacon-2021560 <cchacon-2021560@kinal.edu.gt>
2026-02-22 23:08:23 -0600

    Feat: Definir modelo de usuario

---
+++ src/users/users.model.js
@@ -0,0 +1,50 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+        maxLength: [25, 'Name cannot exceed 25 characters']
+    },
+    surname: {
+        type: String,
+        required: [true, 'Surname is required'],
+        maxLength: [25, 'Surname cannot exceed 25 characters']
+    },
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        unique: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    profilePicture: {
+        type: String 
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    verificationToken: {
+        type: String
+    }
+}, {
+    timestamps: true,
+    versionKey: false
+});
+
+userSchema.methods.toJSON = function() {
+    const { password, verificationToken, __v, ...user } = this.toObject();
+    return user;
+};
+
+export default model('User', userSchema);
